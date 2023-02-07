a5c22ea0665439adcba3cc09aeea13ef74666d63
LeKZanGx <84642805+LeKZanGx@users.noreply.github.com>
2023-02-08 03:14:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -116,3 +116,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
     };
     requestAnimationFrame(update);
 });
+ jQuery(document).ready(function($){
+  $('.superwheel').superWheel({
+    slices: [
+	  {
+	    text: '20% OFF',
+	    message: 'You win 20% off',
+	    value: 1
+	  },{
+	    text: 'No luck',
+	    message: 'You have No luck today',
+	    value: 0
+	  },{
+	    text: '30% OFF',
+	    message: 'You win 30% off',
+	    value: 1
+	  },{
+	    text: 'Lose',
+	    message: 'You Lose :(',
+	    value: 0
+	  },{
+	    text: '40% OFF',
+	    message: 'You win 40% off',
+	    value: 1
+	  },{
+      text: 'Nothing',
+	    message: 'You get Nothing :(',
+        value: 0
+      }
+    ],
+    selector: 'value'
+  });
+});
